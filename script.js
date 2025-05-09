40ebf269df3768c699535127f7a9db88fd0a365b
Xamid549 <xamidramazanob@gmail.com>
2025-05-09 15:26:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const body=document.getElementsByTagName("body")[0];
+const p=document.getElementById("text");
+
+
+const bg=prompt("Bodyni rangini kiriting");
+
+const col=prompt("Textni rangini kiriting");
+
+body.style="background-color:"+bg; 
+p.style="color:"+col;
